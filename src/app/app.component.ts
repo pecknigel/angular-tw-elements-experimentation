b32ec22fc1077dae09b070a3061d06b18abae103
Nigel Peck <nigel-git@nigelpeck.com>
2023-04-19 07:45:37 -0600

    Configure tw-elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { Select, Datepicker, Input, initTE } from "tw-elements";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'stack-overflow-testing';
+export class AppComponent implements AfterViewInit {
+  title = 'test-tailwind-elements';
+
+  ngAfterViewInit() {
+    initTE({ Datepicker, Input, Select });
+  }
+
+  fruitsList: { name: string[] }[] = [
+    {
+      name: ['Mango', 'Papaya', 'Banana']
+    },
+    {
+      name: ['Mango', 'Papaya', 'Banana']
+    },
+    {
+      name: ['Mango', 'Papaya', 'Banana']
+    }
+  ]
 }
